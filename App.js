9a2a64329f83053d6baee537c92ea80bdb638706
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-08-16 10:27:13 +0530

    Added View component for taking the input and even added a button to it

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your study goal!!!'/>
+        <Button title='Add Goal'/>
+      </View>
       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  appContainer: {
+    padding: 50,
+
+  }
 });
