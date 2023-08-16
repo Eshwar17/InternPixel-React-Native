6ae05cc38a5775d6f8b9a9b73456ba4473cf71b1
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-08-16 10:22:42 +0530

    Cleaned the file and added View component into it

--- App.js
+++ App.js
@@ -3,28 +3,12 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    <Text style={{margin: 16, fontSize: 20, borderColor: 'blue', borderWidth: 2, padding: 16}}>Another text</Text>
-      <Text style={styles.text}>Hello Eshwar! Welcome to React Native App</Text>
-      <Button title='Click Me'/>
-      <StatusBar style="auto" />
+    <View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'black',
-    fontSize: 25,
-    borderColor: '#ccc',
-    borderWidth: 3,
-    padding: 16,
-    margin: 16,
-  },
+  
 });
